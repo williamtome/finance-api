e055246da4e92dc480c6b0507e7b10b1263a8704
William <william.tomenh@gmail.com>
2022-01-22 14:08:40 -0300

    feat(Models/Income): create income model.

--- src/Models/Income.php
+++ src/Models/Income.php
@@ -2,13 +2,58 @@
 
 namespace FinanceApp\Models;
 
-use Doctrine\ORM\Mapping\{Entity, Table};
+use Doctrine\ORM\Mapping\{Column, Entity, GeneratedValue, Id, Table};
+use DateTimeInterface;
 
 /**
  * @Entity
  * @Table(name="income")
  */
-class Income extends BaseModel
+class Income
 {
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private string $id;
 
+    /**
+     * @Column(type="text")
+     */
+    private string $description;
+
+    /**
+     * @Column(type="decimal")
+     */
+    private float $amount;
+
+    /**
+     * @Column(type="string")
+     */
+    private string $date;
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @param float $amount
+     */
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @param string $date
+     */
+    public function setDate(string $date): void
+    {
+        $this->date = $date;
+    }
 }
