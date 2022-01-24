5ecebd50fb5a628c460686cd4aceced31af9d738
William <william.tomenh@gmail.com>
2022-01-23 21:39:42 -0300

    feat(routes): add new endpoints.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
+use FinanceApp\Controllers\DeleteIncomeController;
 use FinanceApp\Controllers\ExpenseController;
 use FinanceApp\Controllers\CreateIncomeController;
 use FinanceApp\Controllers\ListIncomeController;
+use FinanceApp\Controllers\ShowIncomeController;
+use FinanceApp\Controllers\UpdateIncomeController;
 
 return [
     '/listar-receitas' => ListIncomeController::class,
     '/criar-receita' => CreateIncomeController::class,
+    '/visualizar-receita' => ShowIncomeController::class,
+    '/atualizar-receita' => UpdateIncomeController::class,
+    '/excluir-receita' => DeleteIncomeController::class,
     '/listar-despesas' => ExpenseController::class,
 ];
