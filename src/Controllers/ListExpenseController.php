dddfe88731701309f01124caed41fe2a19c431c1
William <william.tomenh@gmail.com>
2022-01-24 22:02:51 -0300

    feat(ListExpenseController): list all expense.

---
+++ src/Controllers/ListExpenseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FinanceApp\Controllers;
+
+use FinanceApp\Controllers\Interface\BaseController;
+
+class ListExpenseController implements BaseController
+{
+    public function processRequest()
+    {
+        // TODO: Implement processRequest() method.
+    }
+}
