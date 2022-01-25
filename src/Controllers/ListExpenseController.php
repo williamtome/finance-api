a2d1aa69531ec706c89b410cab82ddf7bcaddbff
William <william.tomenh@gmail.com>
2022-01-25 06:40:31 -0300

    feat(ListExpenseController): implement list all expenses.

--- src/Controllers/ListExpenseController.php
+++ src/Controllers/ListExpenseController.php
@@ -2,12 +2,31 @@
 
 namespace FinanceApp\Controllers;
 
+use Doctrine\ORM\EntityRepository;
+use Doctrine\Persistence\ObjectRepository;
 use FinanceApp\Controllers\Interface\BaseController;
+use FinanceApp\Infra\EntityManagerCreator;
+use FinanceApp\Models\Expense;
 
 class ListExpenseController implements BaseController
 {
+    private ObjectRepository|EntityRepository $expenseRepository;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $this->expenseRepository = $entityManager->getRepository(Expense::class);
+    }
+
     public function processRequest()
     {
-        // TODO: Implement processRequest() method.
+        $expenses = $this->expenseRepository->findAll();
+
+        print_r($this->serialize($expenses));
+    }
+
+    protected function serialize(array $expenses): bool|string
+    {
+        return json_encode($expenses);
     }
 }
